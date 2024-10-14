e6c8ae7c85c63978bbaf647d2ebdac039c97e40a
Tfreire455m <thiago1.000@outlook.com>
2024-10-14 16:21:03 -0300

    fourth commit

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,17 +1,11 @@
-//import styled
 import styled from "styled-components";
-
-//Imports Bootstrap
+import { Link } from "react-router-dom"; // Import Link for routing
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-//css customizado
 import "./Header.css";
-
-//Logotipo
 import logo from "../../assets/1.png";
 
 const Logo = styled.img`
@@ -23,8 +17,8 @@ function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-dark w-100">
       <Container className="container-flex navbar">
-        <Navbar.Brand href="/home">
-          <Logo src={logo}></Logo>
+        <Navbar.Brand as={Link} to="/home">
+          <Logo src={logo} alt="Company Logo" />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -32,15 +26,15 @@ function Header() {
         />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto navbar">
-            <Nav.Link to="/home" className="custom-link">
+            <Nav.Link as={Link} to="/home" className="custom-link">
               INÍCIO
             </Nav.Link>
             <NavDropdown
               title="PRODUTOS"
               id="collapsible-nav-dropdown"
-              className="custom-dropdown-toggle custom-link   "
+              className="custom-dropdown-toggle custom-link"
             >
-              <NavDropdown.Item to="/produtos">iPhones</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/produtos">iPhones</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
